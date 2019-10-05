a427154b855c11a352a6bfd7fc35b47d0613a9eb
Victor Domiciano <victor.dm3@gmail.com>
2019-10-05 08:57:37 -0300

    Fix Godot project

--- project/car/PlayerCar.gd
+++ project/car/PlayerCar.gd
@@ -1,10 +1,22 @@
 extends RigidBody2D
 
+const MAX_VELOCITY = 200
+const ACCELERATION = 100
+const BULLET_COOLDOWN = .5
+
 func _ready():
 	friction = 0.2
+	$BulletCooldown.wait_time = BULLET_COOLDOWN
 
 func _physics_process(delta):
 	update_movement()
+	handle_shooting()
+
+func handle_shooting():
+	if Input.is_action_just_pressed("shoot"):
+		if $BulletCooldown.time_left == 0:
+			
+		
 
 func update_movement():
 	var vector := Vector2(0, 0)
@@ -15,6 +27,6 @@ func update_movement():
 	if Input.is_action_pressed("rotate_clockwise"):
 		angular_velocity += 0.1
 	if Input.is_action_pressed("rotate_counter_clockwise"):
-		angular_velocity -= 0.1
-	applied_force = vector.normalized() * 100
-	linear_velocity = linear_velocity.normalized() * min(linear_velocity.length(), 200)
+		angular_velocity -= 0
+	applied_force = vector.normalized() * ACCELERATION
+	linear_velocity = linear_velocity.normalized() * min(linear_velocity.length(), MAX_VELOCITY)
