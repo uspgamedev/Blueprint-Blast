d8f84a4707b7f57be8400240fd09eb34c371b3cd
Jairo Honorio <registrofx@gmail.com>
2019-10-05 11:30:32 -0300

    Add PlayerCarWithWheels and PlayerCarWithWheelsNoSpring

--- project/car/PlayerCar.gd
+++ project/car/PlayerCar.gd
@@ -8,6 +8,8 @@ const ACCELERATION = 100
 const BULLET_COOLDOWN = .2
 
 onready var main = get_parent()
+onready var wheel_1 : RigidBody2D = $Wheel1
+onready var wheel_2 : RigidBody2D = $Wheel2
 
 func _ready():
 	friction = 0.2
@@ -15,7 +17,7 @@ func _ready():
 	main.car_refs.append(self)
 
 func _physics_process(delta):
-	update_movement()
+	update_movement_with_wheels()
 	handle_shooting()
 
 func handle_shooting():
@@ -37,6 +39,20 @@ func update_movement():
 	if Input.is_action_pressed("rotate_clockwise"):
 		angular_velocity += 0.1
 	if Input.is_action_pressed("rotate_counter_clockwise"):
-		angular_velocity -= 0
+		angular_velocity -= 0.1
 	applied_force = vector.normalized() * ACCELERATION
 	linear_velocity = linear_velocity.normalized() * min(linear_velocity.length(), MAX_VELOCITY)
+
+func update_movement_with_wheels():
+	var force = 20
+	if Input.is_action_pressed("accelerate"):
+		wheel_1.apply_torque_impulse(force)
+		wheel_2.apply_torque_impulse(force)
+	if Input.is_action_pressed("reverse"):
+		wheel_1.apply_torque_impulse(-force)
+		wheel_2.apply_torque_impulse(-force)
+	
+	if Input.is_action_pressed("rotate_clockwise"):
+		angular_velocity += 0.1
+	if Input.is_action_pressed("rotate_counter_clockwise"):
+		angular_velocity -= 0
