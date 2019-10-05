e61063a55e5e715891357a4de20ea511089ff22f
Victor Domiciano <victor.dm3@gmail.com>
2019-10-05 01:20:20 -0300

    Add ConvexPolygonArea class

---
+++ project/ConvexPolygonArea.gd
@@ -0,0 +1,13 @@
+class_name ConvexPolygonArea
+
+static func get_convex_polygon_area(pool_array : PoolVector2Array) -> float:
+	var area : float
+	var constant : float = 1.0/2
+	var left_summation := 0.0
+	var right_summation := 0.0
+	for i in range(pool_array.size()):
+		var upper_point = pool_array[i]
+		var lower_point = pool_array[(i + 1) % pool_array.size()]
+		left_summation += upper_point.x * lower_point.y
+		right_summation += upper_point.y * lower_point.x
+	return constant * (right_summation - left_summation)
