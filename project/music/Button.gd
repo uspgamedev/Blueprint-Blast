808fcd50a8c0a6ae0780d486181923d8374bdb27
rilifon <pudimdefrutas@gmail.com>
2019-10-06 14:36:43 -0300

    Update piano images

--- project/music/Button.gd
+++ project/music/Button.gd
@@ -1,5 +1,7 @@
 extends Button
 
+export (Texture) var piano_image
+
 var music_maker
 var id : int
 
@@ -10,3 +12,4 @@ func _ready():
 func change_offset():
 	music_maker = get_node("../../../").get_current_node_track()
 	music_maker.update_pitch(id - 1)
+	get_node("../../TextureRect").texture = piano_image
