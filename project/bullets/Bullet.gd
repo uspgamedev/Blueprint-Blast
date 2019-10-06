e5772889dcfe938a185e3ae23401457748462773
Arthur Vieira Barbosa <arthur.vb@gmail.com>
2019-10-06 19:52:44 -0300

    Add missile attribute variation (speed and damage)

--- project/bullets/Bullet.gd
+++ project/bullets/Bullet.gd
@@ -1,23 +1,29 @@
 extends RigidBody2D
 class_name Bullet
 
+enum {MIN, MAX}
+
+onready var area = ConvexPolygonArea.get_convex_polygon_area($CollisionPolygon2D.polygon)
+onready var area_ratio = area / MAX_AREA
+
 const VELOCITY := 1000
 const DAMAGE := 20
 const LIFETIME := 5
 const IMPULSE_FORCE := 200
 const ROTATION_FORCE = 2
 const RADIUS := 400
-
-var shooter
+const VELOCITY_FACTOR := [1.5, 0.2]
+const DAMAGE_FACTOR := [.8, 10]
+const MAX_AREA = 11425
 
 func _ready():
+	printt("area", area)
+	printt("area_ratio", area_ratio)
 	$Lifetime.wait_time = LIFETIME
 	$Lifetime.start()
-	$Lifetime.connect("timeout", self, "obliterate")
-	linear_velocity = Vector2(1, 0).rotated(global_rotation) * VELOCITY
-
-func obliterate():
-	queue_free()
+	$Lifetime.connect("timeout", self, "queue_free")
+	linear_velocity = Vector2(1, 0).rotated(global_rotation) * VELOCITY *\
+		lerp(VELOCITY_FACTOR[MIN], VELOCITY_FACTOR[MAX], area_ratio)
 
 func explode():
 	for car in Global.car_refs:
@@ -28,10 +34,11 @@ func explode():
 		randomize()
 		car.back_wheel.apply_impulse(Vector2(0, 0), -vector.normalized().rotated(PI/2) * impulse * ROTATION_FORCE)
 		car.front_wheel.apply_impulse(Vector2(0, 0), vector.normalized().rotated(PI/2) * impulse * ROTATION_FORCE)
-		var damage = inverse_lerp(0, IMPULSE_FORCE, impulse) * DAMAGE
+		var damage = inverse_lerp(0, IMPULSE_FORCE, impulse) * DAMAGE *\
+			 lerp(DAMAGE_FACTOR[MIN], DAMAGE_FACTOR[MAX], area_ratio)
 		car.apply_damage(damage)
-	obliterate()
+	queue_free()
 
+# warning-ignore:unused_argument
 func _on_Bullet_body_entered(body):
-	if body != shooter:
-		explode()
+	explode()
