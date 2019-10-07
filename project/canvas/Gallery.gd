556122d0813082f1750bd22b80814ad922996010
Jairo Honorio <registrofx@gmail.com>
2019-10-06 23:58:06 -0300

    Add ability to click on car to enter race from gallery

--- project/canvas/Gallery.gd
+++ project/canvas/Gallery.gd
@@ -28,13 +28,17 @@ func _ready():
 	
 	var i = 1
 	for car_maker in Global.car_makers:
-		print("Loading car ", i)
 		i += 1
 		var car = load("res://car/BaseCar.tscn").instance()
 		car.load_car_maker(car_maker)
 		var car_representation = load("res://canvas/CarRepresentation.tscn").instance()
 		car_representation.get_node("CarPosition").add_child(car)
+		car_representation.get_node("Button").connect("pressed", self, "_on_car_clicked", [i-2])
 		$VBoxContainer/Cars/GridContainer.add_child(car_representation)
+
+func _on_car_clicked(index):
+	Global.car_maker = Global.car_makers[index]
+	get_tree().change_scene("res://test/TestRaceWithAI.tscn")
 
 func _on_AddButton_pressed():
 	Global.design_mode = Global.DESIGN_MODE.GALLERY
